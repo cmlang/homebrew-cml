744bd358f984491e70d4635e8a76e2c1b89bca48
Quenio dos Santos <quenio@me.com>
2017-05-12 22:48:30 -0300

    Committing version 0.13 of CML Compiler.

--- cml-compiler.rb
+++ cml-compiler.rb
@@ -1,17 +1,17 @@
 require "formula"
 
 class CmlCompiler < Formula
-  VERSION = "0.12"
+  VERSION = "0.13"
 
   desc "The CML Compiler"
   homepage "http://github.com/cmlang"
   url "https://raw.githubusercontent.com/cmlang/cml-releases/master/cml-compiler/cml-compiler-#{VERSION}.zip"
-  sha256 "4ec144a67dabaff4a7734db13afc836b63b3f323bdb89b2854787b4d0baf3007"
+  sha256 "714549818282563e6ebf2d7ec06c37c3863a73d07604cb49cc12230bbcdb67cf"
 
   def install
-    inreplace "bin/cml", "##PREFIX##", "#{prefix}"
-    prefix.install Dir["*"]
-    bin.install_symlink prefix/"bin/cml"
+    inreplace "bin/cml", "##PREFIX##", "#{prefix}/libexec"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/cml"
   end
 
   test do

f13c3274e9b84dbce632fc39e77cd7d706030a10
Quenio dos Santos <quenio@me.com>
2017-05-12 22:21:36 -0300

    Committing version 0.9 of CML Compiler.

--- cml-compiler.rb
+++ cml-compiler.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 class CmlCompiler < Formula
-  VERSION = "0.8"
+  VERSION = "0.9"
 
   desc "The CML Compiler"
   homepage "http://github.com/cmlang"
   url "https://raw.githubusercontent.com/cmlang/cml-releases/master/cml-compiler/cml-package-#{VERSION}-distribution.zip"
-  sha256 "5f18121ab64c7a5ac4d9dc00d64738859d91eb32ceed1f06eb0ea6ae909d4d53"
+  sha256 "052fa45071bfb6e53ff8949be275a3e0bcb6255bc5b5d1a8a2c6e7ac545a7880"
 
   def install
-    inreplace "bin/cml", "##PREFIX##", "#{prefix}"
+    inreplace "bin/cml", "##PREFIX##", "#{prefix}/libexec"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/cml"
   end

a136cc354b63a04f1324f5c3b286685464367909
Quenio dos Santos <quenio@me.com>
2017-10-15 07:47:49 -0200

    Committing version 2017.10.15-alpha of CML Compiler.

--- cml-compiler.rb
+++ cml-compiler.rb
@@ -1,11 +1,11 @@
 
 class CmlCompiler < Formula
-  VERSION = "2017.10.14-alpha"
+  VERSION = "2017.10.15-alpha"
 
   desc "The CML Compiler"
   homepage "https://github.com/cmlang"
   url "https://raw.githubusercontent.com/cmlang/cml-releases/master/cml-compiler/cml-compiler-#{VERSION}.zip"
-  sha256 "7cf446ba326850f22e26f69bcc0b3aad4efd0a220c75596155893b931e6853f5"
+  sha256 "9936387dd5c3ef746755397b239e6a72a174b541956aa062db2cb5a228fe7dfa"
 
   def install
     inreplace "bin/cml", "##PREFIX##", "#{prefix}/libexec"
--- cml-compiler@2017.10.15.rb
+++ cml-compiler@2017.10.15.rb
@@ -1,5 +1,5 @@
 
-class CmlCompilerAT2017 < Formula
+class CmlCompilerAT20171015 < Formula
   VERSION = "2017.10.15-alpha"
 
   desc "The CML Compiler"
